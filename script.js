71fd7de1546a12ba08941964c1668e8a1bcc4965
Alex <axsutter@gmail.com>
2025-04-04 15:06:22 +0200

    Suchverlauf

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const search = document.querySelector("#Search")
 
 const list = document.querySelector("#list")
 
+let suchverlauf = document.querySelector("#Suchverlauf")
+
 let sprache = document.querySelector("#sprache")
 
 let page = document.querySelector("#page")
@@ -19,8 +21,9 @@ highliting.addEventListener("click", function(event){
 })*/
 
 
-
-searchbutton.addEventListener("click", handlesearch)
+searchbutton.addEventListener("click", function(){
+    handlesearch(search.value)
+})
 search.addEventListener("keydown", function(event){
     if (event.key == "Enter"){
         handlesearch()
@@ -29,7 +32,10 @@ search.addEventListener("keydown", function(event){
 
 function highlight(searchvalue, text) {
     console.log(searchvalue)
-    //console.log(text)
+    console.log(text)
+    if (text == null){
+        return text
+    }
     let matchedtextstart = text.search(searchvalue)
     if (matchedtextstart < 0){
         return text
@@ -43,26 +49,25 @@ function highlight(searchvalue, text) {
     return `<div class="matchedtext">${bevormatchedtext}<div class="highlight">${matchedtext}</div>${aftermatchedtext}</div>`
   }
 
-async function handlesearch() {
+async function handlesearch(searchvalue) {
     try{
         list.innerHTML = `<div id="loader"></div>`
-        const pages = await loadartikels()
+        const pages = await loadartikels(searchvalue)
+        console.log("pages",pages)
         mapartiklestohtml(pages)
         if (pages.length <1){
             list.innerHTML = `<div>Nothing found</div>`
         }
     }catch(error){
+        console.log(error)
         list.innerHTML = `<div>ERROR</div>`
     }
-   
+
 }
 
 
 function mapartiklestohtml(pages) {
-    console.log(list.innerHTML)
-    console.log(pages)
     let artikleshtml = []
-    let length = pages.length;
     for (let i = 0; i < pages.length; i++){
         const page = pages[i]
         let highlightettitle = highlight(search.value,page.key)
@@ -77,12 +82,37 @@ function mapartiklestohtml(pages) {
         
     }
     list.innerHTML = artikleshtml.join("")
+    //suchverlauf.innerHTML = `<option>${(search.value)}</option>`
 }
 
-async function loadartikels(){
-        const searchvalue = search.value
+async function loadartikels(searchvalue){
         let url = `https://api.wikimedia.org/core/v1/wikipedia/${sprache.value}/search/page?q=${searchvalue}&limit=${page.value}`;
         let response = await  fetch(url);
         let json = await response.json()
+        let historyshtml = []
+        let history = localStorage.getItem("history") ??""
+        let historyarray = history.split(',')
+        console.log("historynew", historyarray)
+        console.log("history", history)
+        let newhistory = historyarray.push(searchvalue)
+        console.log("newhistory",newhistory)
+        localStorage.setItem("history", newhistory.join(","))
+        for (let i = 0; i < historyarray.length; i++){
+            const historyentri = historyarray[i]
+            historyshtml[i] = `<option class = "suchverlaufbutton">${historyentri}</option>`
+            //console.log(history)
+        }
+        suchverlauf.innerHTML = historyshtml.join("")
+
+        let suchverlaufbuttons = document.querySelectorAll(".suchverlaufbutton")
+        console.log(suchverlaufbuttons)
+        for (let i = 0; i < suchverlaufbuttons.length; i++){
+            const suchverlaufbuttoneinzahl = suchverlaufbuttons[i]
+            suchverlaufbuttoneinzahl.addEventListener("click", async function(){
+                const pages = await handlesearch(suchverlaufbuttoneinzahl.value)
+                mapartiklestohtml(pages)
+                console.log(suchverlaufbuttoneinzahl.value)
+            })
+        }
         return json.pages
 }
